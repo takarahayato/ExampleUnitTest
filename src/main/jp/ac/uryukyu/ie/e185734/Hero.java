536e03be5c1e422eb35cb6b263060ee3afeaf986
takarahayato <e185734@ie.u-ryukyu.ac.jp>
2018-11-15 14:39:06 +0900

    最初のコミット

--- src/main/jp/ac/uryukyu/ie/e185734/Hero.java
+++ src/main/jp/ac/uryukyu/ie/e185734/Hero.java
@@ -1,4 +1,4 @@
-package jp.ac.uryukyu.ie.tnal;
+package jp.ac.uryukyu.ie.e185734;
 
 /**
  * ヒーロークラス。
--- src/main/jp/ac/uryukyu/ie/e185734/Main.java
+++ src/main/jp/ac/uryukyu/ie/e185734/Main.java
@@ -1,4 +1,4 @@
-package jp.ac.uryukyu.ie.tnal;
+package jp.ac.uryukyu.ie.e185734;
 
 /**
  * Created by tnal on 2016/11/13.
